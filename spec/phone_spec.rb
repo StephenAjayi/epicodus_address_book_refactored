14dd3e2581ae7f6ff23940ce058f3d3c2718e94c
stephen & steven francisco <student@epicodus.com>
2015-06-04 14:24:59 -0700

    add spec and code for add, save, and clear methods for Phone class.

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -15,4 +15,27 @@ describe(Phone) do
       expect(test_phone.type()).to(eq("cell"))
     end
   end
+
+  describe('.all') do
+    it('starts out empty at first') do
+      expect(Phone.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a phone number to a contact') do
+      test_phone = Phone.new(:number => 503-333-3333, :type => "cell")
+      test_phone.save()
+      expect(Phone.all()).to(eq([test_phone]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties all save numbers from array') do
+      test_phone = Phone.new(:number => 503-333-3333, :type => "cell")
+      test_phone.save()
+      Phone.clear()
+      expect(Phone.all()).to(eq([]))
+    end
+  end
 end
